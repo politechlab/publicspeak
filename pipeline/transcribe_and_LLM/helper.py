2645b32a5dac8898f85d73b3587ae2906ff36a5e
Tianliang Xu <tianlix@umich.edu>
2025-02-28 14:38:12 +0000

    delete useless import

--- pipeline/transcribe_and_LLM/helper.py
+++ pipeline/transcribe_and_LLM/helper.py
@@ -4,42 +4,12 @@ from typing import Optional, List, Dict, Any
 import time
 import psutil
 import re
-import matplotlib.pyplot as plt
 import whisper
 from whisperx import load_align_model, align
 from whisperx.diarize import DiarizationPipeline, assign_word_speakers
 import json
 import sys
-
-def download_youtube_video(url: str, output_path: Optional[str] = None) -> None:
-    """
-    Downloads a YouTube video from the given URL and saves it to the specified output path or the current directory.
-    Args:
-        url: The URL of the YouTube video to download.
-        output_path: The path where the downloaded video will be saved. If None, the video will be saved to the current
-        directory.
-    Returns:
-        None
-    """
-    yt = YouTube(url)
-
-    video_stream = (
-        yt.streams.filter(progressive=True, file_extension="mp4")
-        .order_by("resolution")
-        .desc()
-        .first()
-    )
-    
-    video_name = video_filename(video_stream.title) + ".mp4"
-
-    if output_path:
-        video_stream.download(output_path, filename=video_name)
-        print(f"Video successfully downloaded to {output_path}")
-    else:
-        video_stream.download(filename=video_name)
-        print("Video successfully downloaded to the current directory")
         
-
 def convert_to_wav(input_file: str, output_file: Optional[str] = None) -> None:
     """
     Converts an audio file to WAV format using FFmpeg.
