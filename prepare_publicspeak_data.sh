9fb83d9edb9739dadb89fe38ef377a028b6e4e97
Tianliang Xu <tianlix@umich.edu>
2025-06-05 01:12:58 +0000

    prepare_publicspeak_data.sh changed

--- prepare_publicspeak_data.sh
+++ prepare_publicspeak_data.sh
@@ -1,3 +1,10 @@
-python pipeline/transcribe_and_LLM/runner_get_llm_pred.py  && \
-python pipeline/PLM/finetuned_one_val_out.py  && \
-python pipeline/generate_processed_data/generate_processed_data.py 
+city="AA"
+model_name="roberta-large"
+lr=2e-5
+epoch=7
+seed=42
+batch_size=4
+
+python pipeline/transcribe_and_LLM/runner_get_llm_pred.py --city ${city}  && \
+python pipeline/PLM/finetuned_one_val_out.py --model_name ${model_name} --city ${city} --lr ${lr} --epoch ${epoch} --seed ${seed} --batch_size ${batch_size} && \
+python pipeline/generate_processed_data/generate_processed_data.py --city ${city} --plm_file_name ${city}"_pred_LOO_roberta.json" --city ${city}
